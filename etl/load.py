2954a4efe3ae0ba97fa2d984239a45ef028134b9
susabhavani987 <vijaychava101@gmail.com>
2025-06-10 12:56:43 -0400

    hello

--- etl/load.py
+++ etl/load.py
@@ -2,8 +2,8 @@ import sqlite3
 import shutil
 
 def load(df, db_path="output.db"):
-    shutil.copy("./mydb.sqlite", "/tmp/mydb.sqlite")
-    conn = sqlite3.connect("/tmp/mydb.sqlite")
+    shutil.copy("./output.db", "/tmp/output.db")
+    conn = sqlite3.connect("/tmp/output.db")
     ##conn = sqlite3.connect(db_path)
     df.to_sql("cleaned_data", conn, if_exists="replace", index=False)
     conn.close()
