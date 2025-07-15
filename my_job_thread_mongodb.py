a77fefd02d13c35244d4fe5f6b6e10fc48ef21ba
susabhavani987 <vijaychava101@gmail.com>
2025-07-14 21:19:19 -0400

    hello

--- my_job_thread_mongodb.py
+++ my_job_thread_mongodb.py
@@ -9,7 +9,9 @@ def main():
     print(f"hello pss {mongo_password}")
     # MongoDB URI with f-string interpolation
     ##uri = f"mongodb+srv://Vijaychava101:{mongo_password}@school.6nof4yc.mongodb.net/?retryWrites=true&w=majority&appName=school"
-    uri = f"mongodb+srv://vijaychava101:45660Living@school.6nof4yc.mongodb.net/?retryWrites=true&w=majority&appName=school"
+    uri = f"mongodb+srv://vijaychava101:45660Living@school.6nof4yc.mongodb.net/?retryWrites=true&w=majority"
+   
+
     print("Connecting to:", uri)
     ##uri = "mongodb+srv://vijaychava101:QAMyb1exS4BNHooQ@school.6nof4yc.mongodb.net/?retryWrites=true&w=majority&appName=school"
 
