6b6a35a927ea18bb1a90607b71541af0e6b6d973
susabhavani987 <vijaychava101@gmail.com>
2025-07-15 11:44:23 -0400

    hello

--- my_job_thread_mongodb.py
+++ my_job_thread_mongodb.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 def main():
     # Get password from environment variable
     mongo_password = os.getenv("MONGO_TOK")
-    print(f"hello pss {mongo_password}")
+    print(f"hello s {mongo_password}")
     # MongoDB URI with f-string interpolation
     uri = f"mongodb+srv://Vijaychava101:{mongo_password}@school.6nof4yc.mongodb.net/?retryWrites=true&w=majority&appName=school"
     ##uri = f"mongodb+srv://vijaychava101:45660Living@school.6nof4yc.mongodb.net/?retryWrites=true&w=majority"
