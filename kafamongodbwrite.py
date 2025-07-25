1a3a739d071ae0e0120e7620f70ba265ec700b96
susabhavani987 <vijaychava101@gmail.com>
2025-07-25 12:53:34 -0400

    hello yes

--- kafamongodbwrite.py
+++ kafamongodbwrite.py
@@ -1,68 +1,32 @@
 
 import sys
 from pyspark.sql import SparkSession
-from pymongo import MongoClient
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StringType, StructType
 import sys
-print(sys.executable)
 
-sys.path.append(r"C:\exegithub\lib\site-packages")
-from kafka import KafkaConsumer
-test = "45660Living"
-schema = StructType().add("name", StringType()).add("age", StringType())
-uri = f"mongodb+srv://vijaychava101:{test}@school.6nof4yc.mongodb.net/Trust?retryWrites=true&w=majority&authSource=admin"
+from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
-        .appName("MyMongoSparkApp") \
-        .master("local[2]") \
-        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.1.1") \
-        .config("spark.mongodb.write.connection.uri", uri) \
-        .getOrCreate()
-print(f"this is spark first")
+    .appName("KafkaDebug") \
+    .master("local[2]") \
+    .getOrCreate()
 
-df_kafka = spark.readStream.format("kafka") \
-          .option("kafka.bootstrap.servers", "d1uqmc63h0primvt047g.any.us-east-1.mpx.prd.cloud.redpanda.com:9092") \
-          .option("subscribe", "demo-topic") \
-          .option("security_protocol", "SASL_SSL") \
-          .option("sasl_mechanism","SCRAM-SHA-256") \
-          .option("sasl_plain_username","Ghattamaneni") \
-          .option("sasl_plain_password","Livingstone#") \
-          .option("kafka.sasl.jaas.config","org.apache.kafka.common.security.scram.ScramLoginModule required username='Ghattamaneni' password='Livingstone#';") \
-          .load()
-print(f"this is kafka second") 
-df_kafka.printSchema()
+spark.sparkContext.setLogLevel("DEBUG")
 
-debug_query = df_kafka \
-    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "topic", "partition", "offset") \
-    .writeStream \
-    .outputMode("append") \
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "d1uqmc63h0primvt047g.any.us-east-1.mpx.prd.cloud.redpanda.com:9092") \
+    .option("subscribe", "demo-topic") \
+    .option("security.protocol", "SASL_SSL") \
+    .option("sasl.mechanism", "SCRAM-SHA-256") \
+    .option("kafka.sasl.jaas.config",
+            "org.apache.kafka.common.security.scram.ScramLoginModule required username='Ghattamaneni' password='Livingstone#';") \
+    .option("kafka.request.timeout.ms", "60000") \
+    .option("kafka.metadata.max.age.ms", "30000") \
+    .load()
+
+df.selectExpr("CAST(value AS STRING)").writeStream \
     .format("console") \
-    .option("truncate", False) \
-    .start()
+    .start() \
+    .awaitTermination()
 
-debug_query.awaitTermination(30) 
 
-df_kafka.selectExpr(
-                    "CAST(key AS STRING)", 
-                    "CAST(value AS STRING)", 
-                    "topic", "partition", "offset"  
-                    ).writeStream \
-                    .format("console") \
-                    .option("truncate", False) \
-                    .start() \
-                    .awaitTermination(60)
-    
-json_df = df_kafka.selectExpr("CAST(value AS STRING) as json") \
-            .select(from_json(col("json"), schema).alias("data")) \
-            .select("data.*")
-print(f"this is kafka json_df")
-query = json_df.writeStream \
-        .format("mongodb") \
-        .option("checkpointLocation", "/tmp/kafka-mongo-checkpoint") \
-        .option("database", "Trust") \
-        .option("collection", "students") \
-        .trigger(processingTime="10 seconds") \
-        .start()
-
-query.awaitTermination(120)
---
+++ kafamongodbwrite_orginal_withtimeout.py
@@ -0,0 +1,68 @@
+
+import sys
+from pyspark.sql import SparkSession
+from pymongo import MongoClient
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StringType, StructType
+import sys
+print(sys.executable)
+
+sys.path.append(r"C:\exegithub\lib\site-packages")
+from kafka import KafkaConsumer
+test = "45660Living"
+schema = StructType().add("name", StringType()).add("age", StringType())
+uri = f"mongodb+srv://vijaychava101:{test}@school.6nof4yc.mongodb.net/Trust?retryWrites=true&w=majority&authSource=admin"
+
+spark = SparkSession.builder \
+        .appName("MyMongoSparkApp") \
+        .master("local[2]") \
+        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.1.1") \
+        .config("spark.mongodb.write.connection.uri", uri) \
+        .getOrCreate()
+print(f"this is spark first")
+
+df_kafka = spark.readStream.format("kafka") \
+          .option("kafka.bootstrap.servers", "d1uqmc63h0primvt047g.any.us-east-1.mpx.prd.cloud.redpanda.com:9092") \
+          .option("subscribe", "demo-topic") \
+          .option("security_protocol", "SASL_SSL") \
+          .option("sasl_mechanism","SCRAM-SHA-256") \
+          .option("sasl_plain_username","Ghattamaneni") \
+          .option("sasl_plain_password","Livingstone#") \
+          .option("kafka.sasl.jaas.config","org.apache.kafka.common.security.scram.ScramLoginModule required username='Ghattamaneni' password='Livingstone#';") \
+          .load()
+print(f"this is kafka second") 
+df_kafka.printSchema()
+
+debug_query = df_kafka \
+    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "topic", "partition", "offset") \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option("truncate", False) \
+    .start()
+
+debug_query.awaitTermination(30) 
+
+df_kafka.selectExpr(
+                    "CAST(key AS STRING)", 
+                    "CAST(value AS STRING)", 
+                    "topic", "partition", "offset"  
+                    ).writeStream \
+                    .format("console") \
+                    .option("truncate", False) \
+                    .start() \
+                    .awaitTermination(60)
+    
+json_df = df_kafka.selectExpr("CAST(value AS STRING) as json") \
+            .select(from_json(col("json"), schema).alias("data")) \
+            .select("data.*")
+print(f"this is kafka json_df")
+query = json_df.writeStream \
+        .format("mongodb") \
+        .option("checkpointLocation", "/tmp/kafka-mongo-checkpoint") \
+        .option("database", "Trust") \
+        .option("collection", "students") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+
+query.awaitTermination(120)
